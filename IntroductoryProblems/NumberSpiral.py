2b38f4aca32a5d75ab488630ac4c90e4565a1951
rhit-nagalab <98065628+rhit-nagalab@users.noreply.github.com>
2026-05-22 17:31:31 -0400

    Worked on NumberSpiral.py
    Getting correct answers but time limit exceeded.

---
+++ IntroductoryProblems/NumberSpiral.py
@@ -0,0 +1,48 @@
+def print_number(y: int, x: int) -> None:
+
+    x_bigger = None
+    square_index = None
+    if y>x:
+        square_index = y
+        x_bigger = False
+    else:
+        square_index = x
+        x_bigger = True
+    
+    sum = None
+    for i in range(1,square_index+1):
+        if i == 1:
+            sum = 1
+            continue
+        
+        sum = sum + 2*(i-1)
+    
+    if square_index % 2 == 0:
+        # Increasing down/left
+        if x_bigger:
+            #Change y/row
+            sum -= abs(square_index - y)
+        else:
+            #Change x/column
+            sum += abs(square_index - x)
+
+    else:
+        #Increasing up/right
+        if x_bigger:
+            #Change y/row
+            sum += abs(square_index - y)
+        else:
+            #Change x/column
+            sum -= abs(square_index - x)
+
+    print(sum)
+        
+
+
+if __name__ == "__main__":
+    t = int(input())
+
+    for _ in range(t):
+        y,x = map(int, input().split(" "))
+
+        print_number(y,x)
