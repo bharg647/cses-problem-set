03e576fb0bf4526eed6278e99460b53e8d8ae39d
rhit-nagalab <98065628+rhit-nagalab@users.noreply.github.com>
2026-05-22 16:39:14 -0400

    Finished Permutations.py
    Used deque as appendleft is O(1) instead of list.insert

--- IntroductoryProblems/Permutations.py
+++ IntroductoryProblems/Permutations.py
@@ -1,4 +1,4 @@
-
+from collections import deque
 
 def print_permutation(n: int) -> None:
     
@@ -9,26 +9,21 @@ def print_permutation(n: int) -> None:
         print("NO SOLUTION")
         return
 
-    arr = []
-    for x in range(1,n+1):
-        arr.append(x)
-
-
     flip = False
-    permutation = []
+    permutation = deque([])
 
-    for i in range((len(arr)+1)//2):
-        if arr[i] == arr[len(arr)-i-1]:
-            permutation.insert(i, arr[i])
+    for i in range(1,((n+1)//2)+1):
+        if i == n-i+1:
+            permutation.insert(i-1, i)
             break
         
         if not flip:
-            permutation.insert(0, arr[i])
-            permutation.append(arr[len(arr)-i-1])
+            permutation.appendleft(i)
+            permutation.append(n-i+1)
             flip = True
         else:
-            permutation.insert(0, arr[len(arr)-i-1])
-            permutation.append(arr[i])
+            permutation.appendleft(n-i+1)
+            permutation.append(i)
             flip = False
     
     print(" ".join(map(str, permutation)))
