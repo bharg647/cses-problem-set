5c671df9271d89212e8a93573cee3e748036feb3
Bhargav Nagalamadaka <38589200+bharg647@users.noreply.github.com>
2026-05-29 22:26:17 -0400

    Finished TwoKnights.py

--- IntroductoryProblems/TwoKnights.py
+++ IntroductoryProblems/TwoKnights.py
@@ -1,9 +1,16 @@
-
+import math
 
 def print_num_ways(n: int) -> None:
 
     for k in range(1,n+1):
-
+        if k == 1:
+            print(0)
+            continue
+        
+        area = k ** 2
+        sum = math.factorial(area)/(math.factorial(2)*math.factorial(area-2))
+        sum -= 4 * (k - 1) * (k - 2)
+        print(sum)
         
 
 if __name__ == "__main__":
